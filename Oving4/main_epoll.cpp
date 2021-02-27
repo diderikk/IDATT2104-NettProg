1a2098e6636dc0f5b6b3c86877086662f62a949e
Diderik Kramer <diderikk@stud.ntnu.no>
2021-02-27 17:10:56 +0100

    Added epoll variation

--- Oving4/main_epoll.cpp
+++ Oving4/main_epoll.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
 #include <list>
 #include <atomic>
 #include <vector>
@@ -9,8 +11,35 @@
 #include <condition_variable>
 
 using namespace std;
-// g++ main_sleep.cpp -lpthread -o main_sleep
-// TODO comment, epoll
+
+struct event_task{
+    epoll_event timeout;
+    std::function<void()> task;
+    event_task(epoll_event timeout, std::function<void()> task): timeout(timeout), task(task){};
+};
+
+epoll_event create_event(int ms)
+{
+    epoll_event timeout;
+    timeout.events = EPOLLIN;
+    // Tiden før "epfd" blir satt til klar, og kan brukes
+    timeout.data.fd = timerfd_create(CLOCK_MONOTONIC, 0);
+    // Setter opp tiden før "file descriptoren" er tilgjengelig
+    //Tatt fra presentasjon
+    itimerspec ts;
+    ts.it_value.tv_sec = ms / 1000;
+    ts.it_value.tv_nsec = (ms % 1000) * 1000000;
+    ts.it_interval.tv_sec = 0;
+    ts.it_interval.tv_nsec = 0;
+
+    // Endrer tiden i "event"
+    timerfd_settime(timeout.data.fd, 0, &ts, nullptr);
+
+    return timeout;
+}
+
+// g++ main_epoll.cpp -lpthread -o main_epoll
+//TODO add a Workers.hpp?
 class Workers
 {
     int thread_size;
@@ -19,6 +48,10 @@ class Workers
     mutex worker_mutex;
     condition_variable worker_cv;
     atomic<bool> stop_var{false};
+    int epfd = epoll_create1(0);
+    vector<epoll_event> buffer;
+    mutex epoll_mutex;
+    vector<event_task> events;
     //Starts all threads, used in start()
     void create_threads(int)
     {
@@ -28,12 +61,39 @@ class Workers
                 thread_task();
             });
         }
+        cout << buffer.size() <<endl;
+        threads.emplace_back([this] {
+            while (true)
+            {
+                // Venter på et event som finnes i den epollen vi lagde tidligere, gitt ved epfd
+                // Vil gå gjennom alle eventsene, og gi tilgang når den mottar "file descriptorene"
+                // -1 som argument, mener at den vil aldri stoppe å polle.
+                auto count = epoll_wait(epfd, buffer.data(), buffer.size(), -1);
+                {
+                    unique_lock<mutex> lock(epoll_mutex);
+                    for (int i = 0; i < count; i++)
+                    {
+                        cout << count << endl;
+                        for (auto &event : events)
+                        {
+                            if (buffer[i].data.fd == event.timeout.data.fd)
+                            {
+                                post(event.task);
+                                epoll_ctl(epfd, EPOLL_CTL_DEL, event.timeout.data.fd, nullptr);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     };
     //Defined under class
     void thread_task();
 
 public:
-    Workers(int n) : thread_size(n){};
+    Workers(int n) : thread_size(n){
+        buffer.resize(50);
+    };
     //Starts all threads, amount defined in constructor
     void start()
     {
@@ -58,27 +118,13 @@ public:
             thread.join();
         }
     }
-    // Use existing threads for timeout functions
-    void post_timeout_sleep(function<void()> task, int ms)
-    {
-        {
-            unique_lock<mutex> lock(worker_mutex);
-            tasks.emplace_back([ms, task, this] {
-                this_thread::sleep_for(chrono::milliseconds(ms));
-                // task();
-                post(task);
-            });
-        }
-        worker_cv.notify_one();
-    }
     //Creates new threads for timeout functions
     void post_timeout(function<void()> task, int ms)
     {
-        threads.emplace_back([ms, task,this] { //Add "this" to arguments if task should be done by worker threads
-            this_thread::sleep_for(chrono::milliseconds(ms));
-            // task();
-            post(task);
-        });
+        auto e = create_event(ms);
+        event_task et {e, task};
+        events.emplace_back(et);
+        epoll_ctl(epfd, EPOLL_CTL_ADD, et.timeout.data.fd, &et.timeout);
     }
 };
 
@@ -117,9 +163,9 @@ int main(void)
         });
     }
 
-    workers.post_timeout_sleep([] {
-        cout << this_thread::get_id() << endl;
-    },5000);
+    // workers.post_timeout_sleep([] {
+    //     cout << this_thread::get_id() << endl;
+    // },5000);
 
     workers.post_timeout([] {
         printf("Task TimeoutThread1 Thread %ld\n", this_thread::get_id());
@@ -134,3 +180,4 @@ int main(void)
 
     return 0;
 }
+
